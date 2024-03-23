d5930ef687c2d777d55d59f42e4e06d5ba6d853b
bt0253 <amozosk@gmail.com>
2024-03-24 00:31:38 +0100

    Versión 3.1 - Feature: Hover sobre el PanelBarras

--- src/Grafica/Graficos/GraficaBarras.java
+++ src/Grafica/Graficos/GraficaBarras.java
@@ -6,6 +6,9 @@ import Logica.Asignatura;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionListener;
 import java.util.HashMap;
 
 public class GraficaBarras extends Panel {
@@ -14,14 +17,14 @@ public class GraficaBarras extends Panel {
     private PanelBarras panelBarras;
     private JLabel leyenda;
 
-    public GraficaBarras(HashMap<Asignatura, Double> notas){
+    public GraficaBarras(HashMap<Asignatura, Double> notas) {
         setSize(1080, 450);
         setBackground(Ventana.colorPrimario);
         panelBarras = new PanelBarras(notas);
         panelLeyenda = new JPanel();
         panelLeyenda.setLayout(null);
         panelLeyenda.setBounds(10, 0, 300, getHeight());
-        panelBarras.setBounds(310, 0, 770, getHeight());
+        panelBarras.setLocation(310, 0);
 
         //Panel leyenda
         leyenda = new JLabel("");
@@ -31,7 +34,7 @@ public class GraficaBarras extends Panel {
         leyenda.setBounds(5, 0, panelLeyenda.getWidth() - 5, panelLeyenda.getHeight());
         StringBuilder textoLeyenda = new StringBuilder("<html>");
         for (Asignatura a : notas.keySet()) {
-            textoLeyenda.append("<p style= 'color: rgb("+a.getColor().getRed()+" "+a.getColor().getGreen()+" "+a.getColor().getBlue()+");'>"+ a.getNombre()+"</p>\n");
+            textoLeyenda.append("<p style= 'color: rgb(" + a.getColor().getRed() + " " + a.getColor().getGreen() + " " + a.getColor().getBlue() + ");'>" + a.getNombre() + "</p>\n");
         }
         textoLeyenda.append("</body>");
         leyenda.setText(textoLeyenda.toString());
@@ -42,26 +45,92 @@ public class GraficaBarras extends Panel {
         add(panelBarras);
     }
 
-    public void actualizar(HashMap<Asignatura, Double> notas){
+    public void actualizar(HashMap<Asignatura, Double> notas) {
         panelBarras.actualizar(notas);
         StringBuilder textoLeyenda = new StringBuilder("<html>");
         for (Asignatura a : notas.keySet()) {
-            textoLeyenda.append("<p style= 'color: rgb("+a.getColor().getRed()+" "+a.getColor().getGreen()+" "+a.getColor().getBlue()+");'>"+ a.getNombre()+"</p>\n");
+            textoLeyenda.append("<p style= 'color: rgb(" + a.getColor().getRed() + " " + a.getColor().getGreen() + " " + a.getColor().getBlue() + ");'>" + a.getNombre() + "</p>\n");
         }
         textoLeyenda.append("</html>");
         leyenda.setText(textoLeyenda.toString());
     }
 
-    class PanelBarras extends Panel{
+    class PanelBarras extends Panel {
 
         private HashMap<Asignatura, Double> notas;
+        private HashMap<Asignatura, JLabel> labels;
+        private HashMap<Integer, Asignatura> indices;
 
-        public PanelBarras(HashMap<Asignatura, Double> notas){
+        public PanelBarras(HashMap<Asignatura, Double> notas) {
+            setSize(770, 450);
             this.notas = notas;
+            labels = new HashMap<>();
+            indices = new HashMap<>();
+            int i = 0;
+            for (Asignatura a : notas.keySet()) {
+                double nota = notas.get(a);
+                if (nota != -1) {
+                    indices.put(i, a);
+                    JLabel label = new JLabel(String.valueOf(nota));
+                    label.setVisible(false);
+                    label.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 12));
+                    label.setForeground(a.getColor());
+                    label.setBackground(Color.green);
+                    label.setHorizontalAlignment(SwingConstants.CENTER);
+                    label.setBounds(10 + i * 35, getHeight() - (int) (getHeight() * (nota / 10)) - 20, 35, 20);
+                    add(label);
+                    labels.put(a, label);
+                    i++;
+                }
+            }
+
+            addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    for (Asignatura a : labels.keySet()) {
+                        labels.get(a).setVisible(false);
+                    }
+                }
+            });
+
+            addMouseMotionListener(new MouseAdapter() {
+                @Override
+                public void mouseMoved(MouseEvent e) {
+                    for (Asignatura a : labels.keySet()) {
+                        labels.get(a).setVisible(false);
+                    }
+                    int index = (e.getX() - 10) / 35;
+                    if (indices.containsKey(index)) {
+                        labels.get(indices.get(index)).setVisible(true);
+                    }
+                }
+            });
         }
 
-        public void actualizar(HashMap<Asignatura, Double> notas){
+        public void actualizar(HashMap<Asignatura, Double> notas) {
             this.notas = notas;
+            for (Asignatura a : labels.keySet()) {
+                remove(labels.get(a));
+            }
+            indices.clear();
+            labels.clear();
+            int i = 0;
+            for (Asignatura a : notas.keySet()) {
+                double nota = notas.get(a);
+                if (nota != -1) {
+                    indices.put(i, a);
+                    JLabel label = new JLabel(String.valueOf(nota));
+                    label.setVisible(false);
+                    label.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 12));
+                    label.setForeground(a.getColor());
+                    label.setBackground(Color.green);
+                    label.setHorizontalAlignment(SwingConstants.CENTER);
+                    label.setBounds(10 + i * 35, getHeight() - (int) ((getHeight() - 30) * (nota / 10)) - 30, 35, 20);
+                    add(label);
+                    labels.put(a, label);
+                    i++;
+                }
+            }
             this.repaint();
         }
 
@@ -69,25 +138,25 @@ public class GraficaBarras extends Panel {
             super.paintComponent(g);
             int contador = 0;
             double sumaNotas = 0;
-            for(Asignatura a : notas.keySet()){
+            for (Asignatura a : notas.keySet()) {
                 g.setColor(a.getColor());
                 double nota = notas.get(a);
-                if(nota == -1) {
+                if (nota == -1) {
                     nota = 0;
                     contador--;
                 }
-                g.fillRect(10 + contador * 35, getHeight() - (int)(getHeight() * (nota / 10)), 35, (int)(getHeight() * (nota / 10)));
-                Color color = new Color((int)(a.getColor().getRed()*0.6), (int)(a.getColor().getGreen()*0.6), (int)(a.getColor().getBlue()*0.6));
+                g.fillRect(10 + contador * 35, getHeight() - (int) ((getHeight() - 30) * (nota / 10)), 35, (int) ((getHeight() - 30) * (nota / 10)));
+                Color color = new Color((int) (a.getColor().getRed() * 0.6), (int) (a.getColor().getGreen() * 0.6), (int) (a.getColor().getBlue() * 0.6));
                 g.setColor(color);
-                g.drawRect(10 + contador * 35, getHeight() - (int)(getHeight() * (nota / 10)), 35, (int)(getHeight() * (nota / 10)));
+                g.drawRect(10 + contador * 35, getHeight() - (int) ((getHeight() - 30) * (nota / 10)), 35, (int) ((getHeight() - 30) * (nota / 10)));
                 contador++;
                 sumaNotas += nota;
             }
             g.setColor(Ventana.colorTernario);
             Graphics2D g2 = (Graphics2D) g;
-            if(notas.size() != 0) {
+            if (notas.size() != 0) {
                 g2.setStroke(new BasicStroke(2));
-                g.drawLine(0, getHeight() - (int)(getHeight() * (sumaNotas / (contador *10))), getWidth(), getHeight() - (int)(getHeight() * (sumaNotas / (contador *10))));
+                g.drawLine(0, getHeight() - (int) (getHeight() * (sumaNotas / (contador * 10))), getWidth(), getHeight() - (int) (getHeight() * (sumaNotas / (contador * 10))));
             }
         }
     }
